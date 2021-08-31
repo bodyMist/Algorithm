e3e80681109dfaca9d7cf895b5f42f2d66ed0d4c
sky834459@gmail.com <password02>
2021-08-31 10:06:26 +0900

    DP, memoization, Silver3

--- SpecialCourse/[1309]Zoo.cpp
+++ SpecialCourse/[1309]Zoo.cpp
@@ -4,7 +4,8 @@ using namespace std;
 // N=1, 1+2	
 // N=2, 1+4+2
 // N=3, 1+6+6+2
-// N=4, 1+8+18+8+2
+// N=4, 1+8+22+8+2
+// N=5, 1+10+
 // 3 7 15 41
 int memo[100005] = {0,3};
 int main() {
@@ -13,7 +14,7 @@ int main() {
 	int N;
 	cin >> N;
 	for (int i = 2; i <= N; i++) {
-		memo[i] = (memo[i - 1] * 2 - 1) % 1009;
+		memo[i] = (memo[i - 1] * 2 - 1) % 9901;
 	}
 	cout << memo[N];
 	return 0;
