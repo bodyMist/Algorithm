a48110feb79707fb764acfc24f5a97e1e7540ba6
bodymist <sky834459@gmail.com>
2022-08-22 15:58:20 +0900

    incomplete, Gold4

---
+++ 2022_lecture/[5052]Phone_List.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+using namespace std;
+
+class Trie{
+  public:
+    bool isEnd;
+    Trie *phone[10];
+    void insert(char* input);
+    bool find(char* target);
+    Trie(){
+      for(int i = 0; i < 10; i++){
+        isEnd = false;
+        phone[i] = NULL;
+      }
+    }
+};
+void Trie::insert(char* input){
+  if(*input == NULL){
+    isEnd = true;
+    return;
+  }
+  int current = *input - '0';
+  if(phone[current] == NULL){
+    phone[current] = new Trie();
+    isEnd = false;
+  }
+  phone[current]->insert(input+1);
+}
+bool Trie::find(char* target){
+  if(*target == NULL){
+    if(isEnd) return false;
+    return true;
+  }
+  int current = *target - '0';
+  if(phone[current] == NULL)
+    return false;
+  phone[current]->find(target+1);
+}
+
+
+int main(){
+  int testcase;
+  cin >> testcase;
+  Trie trie[100];
+  for(int t = 0; t < testcase; t++){
+    int N;
+    cin >> N;
+    trie[t] = Trie();
+    vector<string> store;
+    for(int j = 0; j < N; j++){
+      string input;
+      cin >> input;
+      store.push_back(input);
+      trie[t].insert(&*input.begin());
+    }
+    for(int i = 0; i < N; i++){
+      if(trie[t].find(&*store[i].begin())){
+        cout << "NO" << endl;
+        break;
+      }
+      if(i == N-1){
+        cout << "YES" << endl;
+      }
+    }
+  }
+  return 0;
+}
