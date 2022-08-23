c58c284cd720f46a75b3ee092caab76cc36b84df
bodyMist <sky834459@gmail.com>
2022-08-23 09:44:25 +0900

    Stack, Silver1

---
+++ 2022_lecture/[2504]Bracket.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+  string input;
+  cin >> input;
+
+  stack<char> s;
+  int result = 0, temp = 1;
+  for(int i = 0; i < input.length(); i++){
+    char pointer = input[i];
+    switch(pointer){
+      case '(':
+        s.push(pointer);
+        temp *= 2;
+        break;
+
+      case '[':
+        s.push(pointer);
+        temp *= 3;
+        break;
+
+      case ')':
+        if(s.top() == '('){
+          s.pop();
+          result += temp;
+          temp /= 2;
+        }
+        break;
+
+      case ']':
+        if(s.top() == '['){
+          s.pop();
+          result += temp; 
+          temp /= 3;
+        }
+        break;
+    }
+  }
+
+  if(!s.empty())  cout << "0";
+  else  cout << result;
+
+  return 0;
+}
